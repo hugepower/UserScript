cb11db639e968ea3098d160d939ff1e65f07a10a
hugepower <yezicccccc@gmail.com>
2020-08-26 23:50:29 +0800

    Add thumbnail style

--- JavbusExpandThumbnail.user.js
+++ JavbusExpandThumbnail.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JavbusExpandThumbnail
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Expand all thumbnails.
 // @author       hugepower
 // @match        https://www.javbus.com/*
@@ -13,13 +13,21 @@
 (function() {
     'use strict';
 
-    function JavbusExpandThumbnail(){
+    function javbusStyle(){
+        var styleElement = document.createElement('style');
+        styleElement.type = 'text/css';
+        styleElement.innerHTML = ".Thumbnail{-webkit-box-shadow: 0 1px 3px rgba(0,0,0,.3)}";
+        document.getElementsByTagName('head')[0].appendChild(styleElement);
+    }
+
+    function javbusExpandThumbnail(){
         var aTags = document.getElementsByClassName('sample-box');
         if(aTags){
             document.getElementById('sample-waterfall').setAttribute('style','text-align:center');
             for(var i = aTags.length - 1; i >= 0; i--){
                 var img = document.createElement('img');
                 img.src = aTags[i].href;
+                img.className = "Thumbnail";
                 document.getElementById('sample-waterfall').insertBefore(img,aTags[i]);
                 document.getElementById('sample-waterfall').removeChild(aTags[i]);
             }
@@ -28,7 +36,8 @@
 
     window.onload = function () {
         if (document.getElementsByClassName('row movie')){
-            JavbusExpandThumbnail();
+            javbusStyle();
+            javbusExpandThumbnail();
         }
     }
 })();
