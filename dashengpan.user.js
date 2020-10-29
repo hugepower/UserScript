06b3570b2c675b72d15f75420f7ee2981f1fe250
hugepower <yezicccccc@gmail.com>
2020-10-30 04:14:05 +0800

    打开错误资源的问题始终无法解决

--- dashengpan.user.js
+++ dashengpan.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         大圣盘
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  跳过大圣盘的扫码关注微信公众号，直接访问资源页面。
 // @author       hugepower
 // @match        https://www.dashengpan.com/detail/*
